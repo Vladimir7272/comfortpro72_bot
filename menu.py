0629af44ff0dd5fb8a4d44a30ce0d7d7a2176f87
Vladimir7272 <sterlingrnb@gmail.com>
2025-06-07 16:04:38 +0500

    Delete menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_menu():
-    buttons = [
-        [KeyboardButton(text="Вызвать мастера")],
-        [KeyboardButton(text="Оформить подписку")],
-        [KeyboardButton(text="Я мастер")]
-    ]
-    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
